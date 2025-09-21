ab548a9d69057633c80d0dff8f55898d480c6ce6
Sudhir Singh Thakur <sudhirthakur4@gmail.com>
2025-09-21 13:01:37 +0530

    small fix

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,17 +5,10 @@ import { Outlet } from "react-router-dom";
 export default function MainLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
       <Navbar />
-
-      {/* Main Content */}
       <main className="flex-1 mt-16">
-        {/* Agar children diye gaye hain to unko render karo,
-            warna Outlet render hoga (routes ke liye) */}
         {children || <Outlet />}
       </main>
-
-      {/* Footer */}
       <Footer />
     </div>
   );
